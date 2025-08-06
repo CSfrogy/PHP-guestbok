3ce2c0b333e59c7858ae8a0b6423e3d6fd11ff11
CSfrogy <lonelyguse@gmail.com>
2025-08-06 17:30:32 +0800

    Connection to Database MySQL

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+
+$dsn = 'mysql:host=localhost;dbname=guestbook';
+$username = 'root';
+$password = '123';
+
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
+
+
+
+try{
+    $pdo = new PDO($dsn, $username, $password, $options);
+    echo "Connected successfully";
+}catch (PDOException $e){
+    echo  "Connection failed: " . $e->getMessage();
+}
